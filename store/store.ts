9e636a54b8762bbfc452470ac9678a2bd8171ba2
Ricardo Cuevas <racm@live.cl>
2024-10-15 01:01:33 -0300

    Add axios and update usersSlice

--- store/store.ts
+++ store/store.ts
@@ -1,22 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-import type { Action, ThunkAction } from "@reduxjs/toolkit";
-import rootReducer from "./ConfigureStore";
+import { configureStore } from '@reduxjs/toolkit';
+import type { Action, ThunkAction } from '@reduxjs/toolkit';
+import rootReducer from './ConfigureStore';
+import devToolsEnhancer from 'redux-devtools-expo-dev-plugin';
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false
     }),
+  devTools: false,
+  enhancers: (getDefaultEnhancers) => getDefaultEnhancers().concat(devToolsEnhancer())
 });
 
 export { store };
 
 // Infer the type of `store`
 export type AppStore = typeof store;
-export type RootState = ReturnType<AppStore["getState"]>;
+export type RootState = ReturnType<AppStore['getState']>;
 // Infer the `AppDispatch` type from the store itself
-export type AppDispatch = AppStore["dispatch"];
+export type AppDispatch = AppStore['dispatch'];
 // Define a reusable type describing thunk functions
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
